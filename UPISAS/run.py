6deb7dcf8d06d3a8b1b3d8464f687fc6470f32f0
Wsrxgh <150687797+Wsrxgh@users.noreply.github.com>
2024-12-05 00:37:18 +0100

    run adaptive strategy

--- UPISAS/run.py
+++ UPISAS/run.py
@@ -1,33 +1,39 @@
-from UPISAS.strategies.swim_reactive_strategy import ReactiveAdaptationManager
+from UPISAS.strategies.adaptive_strategy import WildfireAvoidanceStrategy
+#from UPISAS.strategies.wildfire_strategy import WildFireStrategy
 from UPISAS.exemplar import Exemplar
-from UPISAS.exemplars.swim import SWIM
-import signal
-import sys
+from UPISAS.exemplars.wildfire_exemplar import WildFireExemplar  # 假设 WildFireExemplar 类在 wildfire_exemplar.py 文件中
 import time
+import sys
 
 if __name__ == '__main__':
-    
-    exemplar = SWIM(auto_start=True)
-    time.sleep(3)
-    exemplar.start_run()
-    time.sleep(3)
+
+    wildfire_exemplar = WildFireExemplar(auto_start=True)
+    time.sleep(3) 
+    #wildfire_exemplar.start_run()
+    #time.sleep(3)
+
+
 
     try:
-        strategy = ReactiveAdaptationManager(exemplar)
 
+        #strategy = WildFireStrategy(wildfire_exemplar)
+        strategy = WildfireAvoidanceStrategy(wildfire_exemplar)
         strategy.get_monitor_schema()
         strategy.get_adaptation_options_schema()
         strategy.get_execute_schema()
 
         while True:
-            input("Try to adapt?")
+            input("Try to adapt WildFire?")
+
+            print("\n--- Monitoring WildFire ---")  
             strategy.monitor(verbose=True)
+
             if strategy.analyze():
                 if strategy.plan():
                     strategy.execute()
-            
+
     except (Exception, KeyboardInterrupt) as e:
         print(str(e))
-        input("something went wrong")
-        exemplar.stop_container()
+        print("Stopping WildFire container and exiting...")
+        #wildfire_exemplar.stop_container()
         sys.exit(0)
